5d187d5274abc0e92de54fea8fc487901680ab4f
alimombeni <ali.mombenni@gmail.com>
2024-12-03 15:25:55 +0330

    [feat: change contact section and add new color for bg]

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,9 +1,9 @@
 import '../styles/base.scss';
-import '../styles/contact.scss';
 import '../styles/coollink.scss';
 import '../styles/faq.scss';
 import '../styles/landing.scss';
 import '../styles/loader.scss';
+import '../styles/contact.scss';
 import '../styles/projects.scss';
 import '../styles/reviews.scss';
 import '../styles/skills.scss';
